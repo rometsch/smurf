5787913bb141335f729a2da90836cb7be88d35aa
Thomas Rometsch <thomas.rometsch@gmail.com>
2020-01-20 17:34:31 +0100

    prepare setup.py for pipy

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
-import os
 from setuptools import setup, find_namespace_packages
 
-setup(  name="smurf"
-        ,version="0.1"
-        ,description="scripts to handle simulations"
-        ,author="Thomas Rometsch"
-        ,package_dir={'': 'src'}
-        ,package_data = { "" : ["*.sh", ".shell*"] }
-        ,packages=find_namespace_packages(where="src")
-        ,install_requires=[
-          'argcomplete',
-        ]
-        ,zip_safe=False
-        ,entry_points = {
-            'console_scripts': ['smurf=smurf._command_line_:main'],
-        }
+setup(
+    name="smurf",
+    version="0.1",
+    description="Simulation Management using Unique Resource Fingerprints",
+    author="Thomas Rometsch",
+    author_email="thomas.rometsch@uni-tuebingen.de",
+    url="https://github.com/rometsch/smurf",
+    package_dir={'': 'src'},
+    package_data = { "" : ["*.sh", ".shell*"] },
+    packages=find_namespace_packages(where="src"),
+    install_requires=[
+        'argcomplete',
+    ],
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': ['smurf=smurf._command_line_:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: Unix",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Utilities"
+    ],
+    python_requires='>=3.6',
 )
