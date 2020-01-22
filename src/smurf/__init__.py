824463ecf791e15fc29cadd48735b00801ab5c79
Thomas Rometsch <thomas.rometsch@gmail.com>
2020-01-22 13:44:22 +0100

    add a convinience function to module for getting a mount object

--- src/smurf/__init__.py
+++ src/smurf/__init__.py
@@ -10,3 +10,10 @@ from .config import Config
 def Data(*args, **kwargs):
     from . import simdata_integration
     return simdata_integration.Data(*args, **kwargs)
+
+def Mount(search_pattern, **kwargs):
+    from .mount import Mount
+    from .search import search, remote_path
+    sim = search(search_pattern, remote=True, unique=True)[0]
+    path = remote_path(sim)
+    return Mount(path)
