9c8f8f171131e2c28134b1c5889a534fb356f930
Thomas Rometsch <thomas.rometsch@gmail.com>
2020-02-07 15:44:45 +0100

    remove dots from shell plugin and integration files

--- src/smurf/enable_shell_plugin.sh
+++ src/smurf/enable_shell_plugin.sh
@@ -2,14 +2,14 @@
 SCRIPT_DIR="$(dirname $(realpath $0))"
 HOME_DIR="$HOME/.smurf"
 mkdir -p "$HOME_DIR"
-cp $SCRIPT_DIR/.shell_plugin "$HOME_DIR"
-cp $SCRIPT_DIR/.shell_completion "$HOME_DIR"
+cp $SCRIPT_DIR/shell_plugin "$HOME_DIR"
+cp $SCRIPT_DIR/shell_completion "$HOME_DIR"
 
-echo "$SCRIPT_DIR" > "$HOME_DIR/.script_dir.txt"
+echo "$SCRIPT_DIR" > "$HOME_DIR/script_dir.txt"
 
 # check whether shell integration is already enabled
 echo "To activate the shell plugin for this session, run:"
-echo "source ~/.smurf/.shell_plugin"
+echo "source ~/.smurf/shell_plugin"
 echo "Add the following lines to your rc file:"
 echo "# smurf: enable shell plugin"
-echo "[[ -f ~/.smurf/.shell_plugin ]] && source ~/.smurf/.shell_plugin"
+echo "[[ -f ~/.smurf/shell_plugin ]] && source ~/.smurf/shell_plugin"
