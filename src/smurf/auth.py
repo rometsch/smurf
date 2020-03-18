95c330a9b725638059894853c1b6fc4f243620d9
Thomas Rometsch <thomas.rometsch@gmail.com>
2020-03-18 17:15:07 +0100

    add auth module

--- src/smurf/auth.py
+++ src/smurf/auth.py
@@ -6,8 +6,9 @@ to be available in an HPC context.
 Keys used by smurf are password less which is dangerous because it allows
 any malicious actor to take over remote hosts should the local host
 be compromised.
-To mitigate this risk, smurf uses ssh jails. Using this technique, only a
-single predefined command can be executed on the remote host.
+To mitigate this risk, smurf uses ssh command restriction. 
+Using this technique, only a single predefined command can be executed 
+on the remote host.
 """
 
 import os
@@ -60,4 +61,59 @@ def generate_ssh_key(host):
         Hostname of the remote host to setup keys for.
     """
     priv_key_path, _ = get_key_paths(host)
-    subprocess.run(["ssh-keygen", "-N", "", "-f", priv_key_path], check=True)
+    comment = "'limited smurf access from {}@{}'".format(
+        os.environ["USER"], os.uname()[1])
+    subprocess.run(["ssh-keygen", "-C", comment, "-N",
+                    "", "-f", priv_key_path], check=True)
+
+
+def get_pub_key(host):
+    """ Get the public key data for a host. 
+
+    Parameters
+    ----------
+    host : str
+        Hostname of the remote host.
+
+    Returns
+    -------
+    str
+        Public key and comment.
+    """
+    _, pub_key_path = get_key_paths(host)
+    with open(pub_key_path, "r") as infile:
+        content = infile.read().strip()
+    return content
+
+
+def copy_ssh_key(host):
+    """ Copy the smurf key to the remote host and setup command restriction.
+
+    This is done by preparing a temporary file and copying
+    its content using ssh-copy-id.
+
+    Parameters
+    ----------
+    host : str
+        Remote host to register key on.
+    """
+    preprend = 'command="~/.local/bin/smurf --use-ssh-original-command",no-agent-forwarding,no-port-forwarding,no-pty,no-user-rc,no-X11-forwarding'
+    content = preprend + " " + get_pub_key(host)
+    _, pub_key_path = get_key_paths(host)
+    os.rename(pub_key_path, pub_key_path + ".bak")
+    with open(pub_key_path, "w") as outfile:
+        outfile.write(content)
+    subprocess.run(["ssh-copy-id", "-i", pub_key_path, host], check=True)
+    os.rename(pub_key_path + ".bak", pub_key_path)
+
+
+def setup_smurf_ssh_key(host):
+    """ Generates a key for smurf and copy it to the remote host.
+
+    Parameters
+    ----------
+    host : str
+        Hostname of the remote host.
+    """
+    generate_ssh_key(host)
+    copy_ssh_key(host)
